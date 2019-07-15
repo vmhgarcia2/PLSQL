0c9b1500f926c379cda3356b765641c8a5d76772
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-15 14:41:16 +0200

    EjercicioDiaSemana

--- Ejercicio4.sql
+++ Ejercicio4.sql
@@ -2,11 +2,9 @@
 
 DECLARE
     fecha varchar(20):= '&fecha';
-    fecha2 DATE;
+    fecha2 DATE := To_Date(fecha,'yyyy/mm/dd');  
 BEGIN
-    --2019/07/15
-    fecha2 := To_Date(fecha,'yyyy/mm/dd');
-    IF((To_char(fecha2,'DY')='DOM') OR (To_char(fecha2,'DY')='SÁB')) THEN
+    IF((To_char(fecha2,'D')>5)) THEN
         DBMS_OUTPUT.PUT_LINE(fecha2 || ' es fin de semana');
     else     
         DBMS_OUTPUT.PUT_LINE(fecha2 || ' no es fin de semana');
